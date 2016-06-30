feb94bcc5bcdd823ed0c82a493afa70c1645b59a
SGin <marimo59@gmail.com>
2016-06-30 09:59:41 +0900

    unityちゃんを追加 unityちゃんが自動でまっすぐ進むスクリプト、マウスからRayが飛ぶスクリプトを追加

---
+++ Assets/Scripts/Sh_utc_Automove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sh_utc_Automove : MonoBehaviour {
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = new Vector3(0,0,5);
+    }
+}
---
+++ Assets/Sh_Ray.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sh_Ray : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //メインカメラ上のマウスカーソルのある位置からRayを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            //Rayが当たるオブジェクトがあった場合はそのオブジェクト名をログに表示
+            Debug.Log(hit.collider.gameObject.name);
+            //ワールド座標を取得
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                //hit.collider.enabled = false;
+                
+            }
+        }
+        // 4.		
+        // 球形のRayをとばす
+        if (Physics.SphereCast(ray, 5.0f, out hit, 10.0f)) { }
+
+        // 5.		
+        // Rayの可視化
+        Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
+    }
+}
