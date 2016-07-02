3e5bc6a5270b3a65ba544bafdc2bcec368975f49
SGin <marimo59@gmail.com>
2016-07-02 16:00:52 +0900

    UI微修正

---
+++ Assets/Sh_For_Start.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sh_For_Start : MonoBehaviour {
+
+    public void SceneLoad()
+    {
+        Application.LoadLevel("main");
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
